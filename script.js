7233fe809f26d05068c55dcef1dfc8ba12c9c888
Zuha Khundmiri <70804630+Zuha-2109@users.noreply.github.com>
2023-04-03 12:31:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
-
 function palindrome(str){
+	
+ str.toLowerCase();
+ const string = str.toLowerCase();
+ const len = str.length;
 
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        if (string[i] !== string[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
